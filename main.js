8a05bcc04262ca615efde7654eb4133e84ef108f
yama-gen <naoki.aoi.y@gmail.com>
2017-06-02 08:50:33 +0900

    入力項目追加

--- main.js
+++ main.js
@@ -1,18 +1,17 @@
 'use strict';
 
 const { app, BrowserWindow } = require('electron');
-const xlsx = require('xlsx');
 let window;
 
 function createWindow() {
-  window = new BrowserWindow({ width: 800, height: 600});
+  window = new BrowserWindow({ width: 1200, height: 900});
   window.loadURL('file://' + __dirname + '/index.html');
   window.on('closed', () => {window = null;});
 }
 
 app.on('ready', createWindow);
 app.on('window-all-closed', () => {
-  if (process.platform != 'darwin') {
+  if (process.platform !== 'darwin') {
     app.quit();
   }
 });
--- renderer.js
+++ renderer.js
@@ -1,5 +1,6 @@
-var $ = jQuery = require("./node_modules/jquery/dist/jquery.min.js");
-var Hammer = require('./node_modules/hammerjs/hammer.min.js');
+const $ = jQuery = require("./node_modules/jquery/dist/jquery.min.js");
+const Hammer = require('./node_modules/hammerjs/hammer.min.js');
+const xlsx = require('xlsx');
 
 $(document).ready(function() {
     $('select').material_select();
